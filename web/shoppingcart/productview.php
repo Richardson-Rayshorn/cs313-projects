b09ae7ed8a7447e5199ef4046eed97d34b7e0734
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 09:40:23 -0400

    updates shopping styles

--- web/shoppingcart/productview.php
+++ web/shoppingcart/productview.php
@@ -2,10 +2,10 @@
 
 echo '
 <div class="row">
-    <div class="col s12 m12 l6">
+    <div class="col s12 m12 l8">
         <div class="carousel carousel-slider center">
             <div class="carousel-fixed-item center">
-                <a class="btn waves-effect white grey-text darken-text-2">button</a>
+                <a class="btn waves-effect white grey-text darken-text-2">Add to Cart</a>
             </div>
             <div class="carousel-item red white-text" href="#one!">
                 <h2>First Panel</h2>
@@ -25,7 +25,13 @@ echo '
             </div>
         </div>
     </div>
-    <div class="col s12 m6 l6">
-
+    <div class="col s12 m12 l4">
+        <h2>Product Name</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+        est laborum.</p>
+        <p>Price : $0.00</p>
     </div>
 </div>';
