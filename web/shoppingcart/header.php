870b7127357c902837b10718f1175acf4547c1f2
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 09:23:24 -0400

    updates shopping styles

--- web/shoppingcart/header.php
+++ web/shoppingcart/header.php
@@ -10,6 +10,10 @@
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="./styles/index.css">
+      <script
+      src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+      crossorigin="anonymous"></script>
       <!-- Compiled and minified CSS -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
