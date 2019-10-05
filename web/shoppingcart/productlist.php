5f2c29670df3acf77ce71e596a89bdfa0c978f21
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-04 21:47:02 -0400

    shopping cart

---
+++ web/shoppingcart/productlist.php
@@ -0,0 +1,6 @@
+<?php
+
+    $products = array(
+        array("Bread", "made with flour", 4),
+        array("cheese", "made with milk", 8)
+    )
--- web/shoppingcart/products.php
+++ web/shoppingcart/products.php
@@ -1,16 +1,24 @@
 <?php 
-    echo '
-<div class="products">
-    <div>
-        <img src="" alt="product 1" />
-        <div class="product-name">Product 1</div>
-        <div class="product-summary">Description</div>
-        <button class="add-to-cart">Add to Cart</button>
-    </div>
-    <div>
-        <img src="" alt="product 1" />
-        <div class="product-name">Product 1</div>
-        <div class="product-summary">Description</div>
-        <button class="add-to-cart">Add to Cart</button>
-    </div>
-</div> ';
+
+    include_once "productlist.php";
+    $num = count($products);
+
+    echo $num;
+//     echo '
+// <div class="products">'
+//     for($i = 0; )
+//     <div>
+//         <img src="" alt="product 1" />
+//         <div class="product-name">Product 1</div>
+//         <div class="product-summary">Description</div>
+//         <div class="price">$20.00</div>
+//         <button class="add-to-cart" data-id="1">Add to Cart</button>
+//     </div>
+//     <div>
+//         <img src="" alt="product 2" />
+//         <div class="product-name">Product 2</div>
+//         <div class="product-summary">Description</div>
+//         <div class="price">$25.00</div>
+//         <button class="add-to-cart" data-id="2">Add to Cart</button>
+//     </div>
+// </div> ';
