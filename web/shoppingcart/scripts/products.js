755312c67a869dab2e65e0430664695731b3c0cd
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 19:28:43 -0400

    updates shopping php1

--- web/shoppingcart/scripts/products.js
+++ web/shoppingcart/scripts/products.js
@@ -1,6 +1,6 @@
 var app = document.getElementById("app");
 var parentEle = document.getElementById("header-nav");
-var anchor = parentEle.querySelectorAll(".anchorTag");
+var anchor = document.querySelectorAll(".anchorTag");
 
 var xmlhttp = new XMLHttpRequest();
 var xmlcart = new XMLHttpRequest();
