6799a46c1f1abf7b8d92702fbb83c12b93c7ee6c
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 09:27:06 -0400

    updates shopping styles

--- web/shoppingcart/productview.php
+++ web/shoppingcart/productview.php
@@ -1,5 +1,6 @@
-<?php include 'productlist.php'; ?>
+<?php include 'productlist.php'; 
 
+echo '
 <div>
 
     <div>
@@ -28,13 +29,4 @@
     <div>
 
     </div>
-</div>
-
-<!-- <button class="see-product" data-id="'; 
-            echo $data_id;
-            echo'" data-name="';
-            echo $products[$i][1]; echo '"';
-            echo '" data-price ="';
-            echo $products[$i][3]; echo '"
-             onClick="seeProduct(this)">See Product</button>
-            </div>'; -->
+</div>';
