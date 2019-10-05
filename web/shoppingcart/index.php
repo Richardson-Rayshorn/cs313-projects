0088d0f822f07685fc49954441bdc8418c56597c
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 09:14:01 -0400

    updates shopping styles

--- web/shoppingcart/index.php
+++ web/shoppingcart/index.php
@@ -1,4 +1,3 @@
 <?php include_once 'header.php';?>
-    <!-- <main id="app"></main> -->
-<?php include 'products.php'; ?>
+    <main id="app"></main>
 <?php include_once 'footer.php'; ?>
--- web/shoppingcart/scripts/products.js
+++ web/shoppingcart/scripts/products.js
@@ -1,3 +1,16 @@
+var app = document.getElementById("app");
+
+var xmlhttp = new XMLHttpRequest();
+
+xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200) {
+        app.innerHTML =  this.responseText;
+    }
+}
+
+xmlhttp.open("GET", "products.php", true);
+xmlhttp.send();
+
 function seeProduct(e) {
-    alert(e.dataset.id + " " + e.dataset.name + " " + e.dataset.price);
+    alert(e.dataset.id);
 }
