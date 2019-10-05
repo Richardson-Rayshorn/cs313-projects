ed7fd79d34d8af170c7892f2688ef63ebdd1977b
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 07:17:40 -0400

    shopping cart2

--- web/shoppingcart/products.php
+++ web/shoppingcart/products.php
@@ -1,23 +1,31 @@
 <?php include 'productlist.php'; 
-    
-$num = count($products);
 
-    echo $num . "<span>total 2</span>";
-//     echo '
-// <div class="products">'
-//     for($i = 0; )
-//     <div>
-//         <img src="" alt="product 1" />
-//         <div class="product-name">Product 1</div>
-//         <div class="product-summary">Description</div>
-//         <div class="price">$20.00</div>
-//         <button class="add-to-cart" data-id="1">Add to Cart</button>
-//     </div>
-//     <div>
-//         <img src="" alt="product 2" />
-//         <div class="product-name">Product 2</div>
-//         <div class="product-summary">Description</div>
-//         <div class="price">$25.00</div>
-//         <button class="add-to-cart" data-id="2">Add to Cart</button>
-//     </div>
-// </div> ';
+$data_id = 1;
+$num = count($products);
+$numinside = count($products, COUNT_RECURSIVE);
+$numval = $numinside - $num;
+$numres = $numval / $num;
+
+// echo $num . "<span>total 2</span>";
+    echo '
+<div class="products">';
+    for($i = 0; $i < $num; $i++)
+    {
+        for($y = 0; $y < $numres; $y++)
+        {
+            echo '<div>
+            <img src="" alt="product image" />
+            <div class="product-name">'; echo $product[$i][$y];
+            echo '</div>
+            <div class="product-summary">'; echo $product[$i][$y];
+            echo '</div>
+            <div class="price"> $'; echo $product[$i][$y];
+            echo '</div>
+            <button class="add-to-cart" data-id="'; 
+            echo data_id;
+            echo'">Add to Cart</button>
+            </div>';
+        }
+    }
+echo '
+</div> ';
