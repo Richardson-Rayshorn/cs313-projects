e2f34d303b1bcfb10540d7c52193ddbd99d8d827
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 10:44:20 -0400

    updates shopping styles

--- web/shoppingcart/productview.php
+++ web/shoppingcart/productview.php
@@ -7,31 +7,33 @@
     $numval = $numinside - $num;
     $numres = $numval / $num;
     $productname = "didnt collect";
-    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $productname = $_GET['productname'];
+        $productname = $_POST['productname'];
         echo $productname;
     }
     echo '
-    <div class="row">
-        <div class="col s12 m12 l8">
-            <div class="carousel carousel-slider center">
-                <div class="carousel-fixed-item center">
-                    <a class="btn waves-effect white grey-text darken-text-2" onClick="addCart(this)">Add to Cart</a>
+    <form method="post" action="<?php echo $_SERVER[\'PHP_SELF\'];?>" >
+        <div class="row">
+            <div class="col s12 m12 l8">
+                <div class="carousel carousel-slider center">
+                    <div class="carousel-fixed-item center">
+                        <a class="btn waves-effect white grey-text darken-text-2" onClick="addCart(this)">Add to Cart</a>
+                    </div>
+                    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"></a>
                 </div>
-                <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"></a>
+            </div>
+            <div class="col s12 m12 l4">
+                <h4 name="productname">Product Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+                est laborum.</p>
+                <span class="row">
+                    <p class="col s6 m6 l6">Price : $0.00</p>
+                    <p class="col s6 m6 l6">Quantity : 0 </p>
+                </span>
             </div>
         </div>
-        <div class="col s12 m12 l4">
-            <h4 name="productname">Product Name</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-            est laborum.</p>
-            <span class="row">
-                <p class="col s6 m6 l6">Price : $0.00</p>
-                <p class="col s6 m6 l6">Quantity : 0 </p>
-            </span>
-        </div>
-    </div>';
+    </form>';
