928e211daaf31de9a9389fac22b8ee64ba92cdca
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 20:37:19 -0400

    updates shopping php1

--- web/shoppingcart/productlist.php
+++ web/shoppingcart/productlist.php
@@ -2,7 +2,8 @@
 
     $products = array(
         array("bread.jpg","Bread", "made with flour", 4.00),
-        array("cheese2.jpg", "cheese", "made with milk", 8.00)
+        array("cheese2.jpg", "cheese", "made with milk", 8.00),
+        array("sausage.jpg", "sausage", "made with meat", 10.00)
     )
 
 ?>
