a3071846da29762560bdad3a42b7b6cf286d6dd9
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 23:51:52 -0400

    updates shopping php1

--- web/shoppingcart/products.php
+++ web/shoppingcart/products.php
@@ -8,15 +8,6 @@ $numinside = count($products, COUNT_RECURSIVE);
 $numval = $numinside - $num;
 $numres = $numval / $num;
 
-$count_session = count($_SESSION["cart"]);
-for ($i = 0; $i < $count_session; $i++)
-{
-    foreach ($_SESSION["cart"][$i] as $key => $value)
-    {
-        echo $key . " " . $value;
-    }
-}
-
     echo '
 <div class="products row">';
     for($i = 0; $i < $num; $i++)
