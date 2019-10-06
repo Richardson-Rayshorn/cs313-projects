77638ae1b8bb25ab4a3477da4af2399f8f41c00e
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 23:09:53 -0400

    updates shopping php1

--- web/shoppingcart/cart.php
+++ web/shoppingcart/cart.php
@@ -2,23 +2,25 @@
 session_start();
 
 $count_session = count($_SESSION["cart"]);
-for ($i = 0; $i < $count_session; $i++)
-{
-    foreach ($_SESSION["cart"][$i] as $key => $value)
-    {
-        echo '
+
+echo '
         <div class="row">
             <div class="col s12 m12 l12">
                 <h3>Shopping Cart</h3>
             </diV>
             <div class="col s12 m4 l4">';
+for ($i = 0; $i < $count_session; $i++)
+{
+    foreach ($_SESSION["cart"][$i] as $key => $value)
+    {
                 echo $key;
         echo '</div>
             <div class="col s12 m4 l4">';
                 echo $value;
-      echo '</div>
-        </div>';
     }
 }
+
+echo '</div>
+        </div>';
 
 
