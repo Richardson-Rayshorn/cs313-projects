e9a3d4eae3b846cefb6727bdc81488a902191b78
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-05 21:50:54 -0400

    updates shopping php1

--- web/shoppingcart/productview.php
+++ web/shoppingcart/productview.php
@@ -8,18 +8,25 @@
     $numval = $numinside - $num;
     $numres = $numval / $num;
     
-    
     $prodname = $_REQUEST["prodname"];
     $price = $_REQUEST["price"];
     $quantity = $_REQUEST["quan"];
 
-    // if(!isset($_SESSION["cart"]))
-    // {
-    //     $_SESSION["cart"] = Array();
-    // }
-    $_SESSION["cart"] = Array();
+    if(isset($_SESSION['counter']))
+    {
+        $_SESSION['counter'] += 1;
+    }
+    else 
+    {
+        $_SESSION['counter'] = 0;
+    }
 
-    $_SESSION["cart"][$id_int] = array("Product Name"=>$prodname, "Price"=>$price, "Quantity"=>$quantity);
+    if(!isset($_SESSION["cart"]))
+    {
+        $_SESSION["cart"] = Array();
+    }
+
+    $_SESSION["cart"][$_SESSION['counter']] = array("Product Name"=>$prodname, "Price"=>$price, "Quantity"=>$quantity);
     // array_push($_SESSION["cart"][$id_int], $_REQUEST["prodname"],$_REQUEST["price"],$_REQUEST["quan"]);
   
     foreach ($_SESSION["cart"][$id_int] as $key => $value)
