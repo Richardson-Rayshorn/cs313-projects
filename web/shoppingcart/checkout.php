16e40357c0f9f2dd228c183c38007d430d6e6489
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-07 22:38:51 -0400

    updates shopping php1

--- web/shoppingcart/checkout.php
+++ web/shoppingcart/checkout.php
@@ -1,41 +1,39 @@
 <?php
-$urlLink = $_SERVER["PHP_SELF"];
+$urlLink = "confirmation.php";
 
-if (isset($_POST["submit"]))
-{
-    if (empty($_POST["first_name"]))
-    {
-        $fnameErr = "Required";
-    }
-    else if (empty($_POST["last_name"]))
-    {
-        $lnameErr = "Required";
-    }
-    else if (empty($_POST["email"]))
-    {
-        $emailErr = "Required";
-    }
-    else if (empty($_POST["city"]))
-    {
-        $cityErr = "Required";
-    }
-    else if (empty($_POST["country"]))
-    {
-        $countryErr = "Required";
-    }
-    else if (empty($_POST["address"]))
-    {
-        $addressErr = "Required";
-    }
-    else if (empty($_POST["number"]))
-    {
-        $numberErr = "Required";
-    }
-    else 
-    {
-        $urlLink = "confirmation.php";
-    }
-}
+echo '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="./styles/index.css">
+      <script
+      src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+      crossorigin="anonymous"></script>
+      <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+      <!-- Compiled and minified JavaScript -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+              
+      <title>Personal Homepage</title>
+    </head>
+    <body>
+  <header>
+    <nav id="navbar">
+      <div class="nav-wrapper">
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="./index.php">Product</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div class="container">
+    ';
+
 echo '
     <h2>Checkout</h2>
     <p>Please fill in an address below to ship to!</p>
@@ -86,3 +84,20 @@ echo '
         </form>
     </div>
 ';
+
+echo '
+    </div>
+    <footer>
+    <div class="container">
+      <nav>
+        <ul>
+            <li><a href="./index.php">Product</a></li>
+        </ul>
+      </nav>
+    </div>
+    <script src="./scripts/products.js"></script>
+    <script src="./scripts/materialcustom.js"></script>
+  </footer>
+</body>
+</html>
+    ';
