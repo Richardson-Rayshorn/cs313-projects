8bee7aa3de8c94e44d296a075b54a5d6084e9dbf
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-07 21:11:38 -0400

    updates shopping php1

--- web/shoppingcart/checkout.php
+++ web/shoppingcart/checkout.php
@@ -2,51 +2,51 @@
 
 echo '
     <h2>Checkout</h2>
-    <p>Please fill in your address below</p>
+    <p>Please fill in an address below to ship to!</p>
     <div class="row">
-        <form method="post" action="confirmation.php" class="col s12">
+        <form method="post" action="'; echo htmlspecialchars(; echo 'confirmation.php'; echo ); echo'" class="col s12">
             <div class="row">
-                <div class="input-field col s6">
-                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                <div class="input-field col s4">
+                    <input id="first_name" type="text" class="validate" name="first_name">
+                    <span class="error">* '; echo $fnameErr; echo '</span>
                     <label for="first_name">First Name</label>
                 </div>
-                <div class="input-field col s6">
-                    <input id="last_name" type="text" class="validate">
+                <div class="input-field col s4">
+                    <input id="last_name" type="text" class="validate" name="last_name">
+                    <span class="error">* '; echo $lnameErr; echo '</span>
                     <label for="last_name">Last Name</label>
                 </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
+                <div class="input-field col s4">
+                    <input id="email" type="email" class="validate" name="email">
+                    <span class="error">* '; echo $emailErr; echo '</span>
                     <label for="email">Email</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s6">
-                    <input id="city" type="text" class="validate">
+                    <input id="city" type="text" class="validate" name="city">
+                    <span class="error">* '; echo $cityErr; echo '</span>
                     <label for="city">City</label>
                 </div>
                 <div class="input-field col s6">
-                    <input id="country" type="text" class="validate">
+                    <input id="country" type="text" class="validate" name="country">
+                    <span class="error">* '; echo $countryErr; echo '</span>
                     <label for="country">Country</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s6">
-                    <input id="address" type="text" class="validate">
+                    <input id="address" type="text" class="validate" name="address">
+                    <span class="error">* '; echo $addressErr; echo '</span>
                     <label for="address">Address</label>
                 </div>
                 <div class="input-field col s6">
-                    <input id="number" type="tel" class="validate pattern="[0-9]{3}-[0-9]{2}-[0-9]{4}">
+                    <input id="number" type="tel" class="validate pattern="[0-9]{3}-[0-9]{2}-[0-9]{4}" name="number">
+                    <span class="error">*'; echo $numberErr; echo '</span>
                     <label for="number">Telephone Number</label>
                 </div>
             </div>
-            <div class="row">
-                <div class="input-field col s12">
-                    <textarea id="textarea1" class="materialize-textarea"></textarea>
-                    <label for="textarea1">Textarea</label>
-                </div>
-            </div>
+            <input type="submit" name="submit">
         </form>
     </div>
 ';
---
+++ web/shoppingcart/confirmation.php
@@ -0,0 +1,5 @@
+<?php
+    $fnameErr = $lnameErr = $emailErr = $cityErr = $countryErr = $addressErr = $numberErr = "";
+    $fname = $lname = $email = $city = $country = $address = $number = "";
+
+    echo  'Calling the confirmation works';
