22d626761af7aa1d65ca1306b414edcd8cce608d
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-07 21:54:05 -0400

    updates shopping php1

--- web/shoppingcart/confirmation.php
+++ web/shoppingcart/confirmation.php
@@ -27,7 +27,7 @@
     <nav id="navbar">
       <div class="nav-wrapper">
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li>a href="./index.php">Products</a></li>
+            <li><a href="./index.php">Products</a></li>
         </ul>
       </div>
     </nav>
