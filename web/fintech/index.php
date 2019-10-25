e21ef55afcc12be72ab46819b8d94dcdfcfbdd09
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-25 19:18:43 -0400

    database updated files

--- web/fintech/index.php
+++ web/fintech/index.php
@@ -1,5 +1,6 @@
-<?php include_once "header.php"; 
-    $title = "Home";
+<?php 
+$title = "Home";
+include_once "header.php"; 
     echo '<main>
         <section class="jumbotron">
             <section>
