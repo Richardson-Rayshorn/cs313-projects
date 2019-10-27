c0e70fdf92e1880f3cc30575a6ff8c26c5f0a424
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-26 23:33:17 -0400

    database updated files

--- web/fintech/search.php
+++ web/fintech/search.php
@@ -13,6 +13,16 @@ include_once "header.php";
     $offerings->execute();
     $offerings_load = $offerings->fetchAll(PDO::FETCH_ASSOC);
 
+    $query = $db->prepare('SELECT types_name, offerings_name,
+    company_name, company_description
+    FROM ((((institute_types
+    INNER JOIN types ON institute_types.types_id = types.id)
+    INNER JOIN institutions ON institute_types.institutions_id = institutions.id)
+    INNER JOIN institute_offerings ON institutions.id = institute_offerings.institutions_id)
+    INNER JOIN offerings ON institute_offerings.offerings_id = offerings.id)');
+    $query->execute();
+    $business = $query->fetchAll(PDO::FETCH_ASSOC);
+
     $link = htmlspecialchars("./business.php");
     echo '<form method="GET" action="'; echo $link; echo '">
         <div class="row">
@@ -47,5 +57,28 @@ include_once "header.php";
         </div>
         <input type="submit" class="btn" value="Search">
     </form>';
+
+    echo '<div class="row">
+        <!-- Displays the businesses that match
+        the search -->';
+        foreach ($business as $rows)
+        {
+            echo '<div class="col sm12">
+                <h3>';
+                echo $rows['company_name'];
+                echo '</h3>
+                <p> <span> Description</span> <br/>';
+                echo $rows['company_description'];
+                echo '</p>
+                <p> <span> Business type</span> <br/>';
+                echo $rows['types_name'];
+                echo '</p>
+                <p> <span> Offerings</span> <br/>';
+                echo $rows['offerings_name'];
+                echo '</p>
+                <a class="btn" onClick="">Learn More</a>
+            </div>';
+        }
+    echo '</div>';
 
 include_once "footer.php" ; ?>
