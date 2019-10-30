be3405ab641043701f1a21152ae042e9a2a5e703
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-30 01:21:23 -0400

    designing the app

---
+++ web/fintech/business-details.php
@@ -0,0 +1,15 @@
+<?php
+
+echo $_GET['compId'];
+echo '
+<!-- give more details on each business
+and a button that users can click to add the 
+business to their list of interest -->
+
+<div >
+    <h3>Company Name</h3>
+    <p>Company desc</p>
+    <p>contact person</p>
+    <p>number</p>
+    <p>address</p>
+</div>
