b707edf68d77107ad8d8b30505ba67b2ef7d6c82
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-29 21:30:22 -0400

    designing the app

--- web/fintech/header.php
+++ web/fintech/header.php
@@ -18,7 +18,7 @@ $link = htmlspecialchars("./business.php");
       <!-- Compiled and minified JavaScript -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
               
-       
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <title>'; echo $title; echo ' | Fintech</title>
     </head>
     <body>
