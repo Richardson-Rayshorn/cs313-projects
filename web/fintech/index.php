3aff0b45447b6dc7ac4c40981635240fe9fbb178
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-29 23:06:58 -0400

    designing the app

--- web/fintech/index.php
+++ web/fintech/index.php
@@ -7,7 +7,7 @@ include_once "header.php";
             <section class="container">
                 <h2>FUND YOUR FUTURE.</h2>
                 <p>Find the right financial aid for your needs.</p>
-                <a class="btn" href="./search.php">Learn More</a>
+                <a class="btn" href="./search.php">Show me how!</a>
             </section>
         </section>
     </main>';
