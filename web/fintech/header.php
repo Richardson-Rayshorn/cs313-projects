b0c8b9bc852b7699993c53e8eaa1996878b7177f
RichardsonRayshorn <rayshorndc@gmail.com>
2019-10-29 21:37:56 -0400

    designing the app

--- web/fintech/header.php
+++ web/fintech/header.php
@@ -32,13 +32,13 @@ $link = htmlspecialchars("./business.php");
       <div class="nav-wrapper white">
         <a href="#!" class="brand-logo left">FundMine</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><form>
+          <form>
           <div class="input-field">
             <input id="search" type="search" required>
             <label class="label-icon" for="search"><i class="material-icons">search</i></label>
             <i class="material-icons">close</i>
           </div>
-        </form></li>
+        </form>
           <li><a href="./signup">Signup</a></li>
         </ul>
       </div>
