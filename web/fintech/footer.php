a0d4d52edb6d4b696a22dd7efb240be0d0bb15b1
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-02 17:45:42 -0400

    designing the app

--- web/fintech/footer.php
+++ web/fintech/footer.php
@@ -3,17 +3,7 @@
     </main>
     <footer class="grey darken-4">
       <div class="container">
-        <nav>
-          <ul>';
-          for($i = 0; $i < $arr_length; $i++) {
-            echo '<li><a class="anchorTag" id="'; 
-            echo $menu_links[$i]; 
-            echo '"';
-            echo $menu_links[$i]; 
-            echo '</a></li>';
-          }
-          echo '</ul>
-        </nav>
+        
       </div>
     </footer>
     <script src="./scripts/material.js"></script>
