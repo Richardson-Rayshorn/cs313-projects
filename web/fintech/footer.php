dea979f17cfe52f9844808f81baace39873d17bf
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-02 17:50:24 -0400

    designing the app

--- web/fintech/footer.php
+++ web/fintech/footer.php
@@ -3,7 +3,15 @@
     </main>
     <footer class="grey darken-4">
       <div class="container">
-        
+        <div id="footer-nav">
+          <ul>
+            <li>Signup</li>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div id="copyright">
+          <p>&copy Eknotec Services</p>
+        </div>
       </div>
     </footer>
     <script src="./scripts/material.js"></script>
