564a75f7fa2814ae017bb5ca942a1ec7e25b9fae
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-02 17:15:13 -0400

    designing the app

--- web/fintech/header.php
+++ web/fintech/header.php
@@ -19,9 +19,9 @@ $link = htmlspecialchars("./business.php");
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
               
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <title>'; echo $title; echo ' | Fintech</title>
+      <title>'; echo $title; echo ' | FundMine</title>
     </head>
-    <body class='; echo $page; echo'>
+    <body class=''>
   <header class="white">
     <nav class="white">
       <div class="nav-wrapper">
