a5e0b5fc72d212da757d39f3da0266ccc45b5294
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-02 17:41:07 -0400

    designing the app

--- web/fintech/footer.php
+++ web/fintech/footer.php
@@ -1,23 +1,23 @@
 <?php 
     echo '
-    </div>
+    </main>
     <footer>
-    <div class="container">
-      <nav class="grey darken-4">
-        <ul>';
-        for($i = 0; $i < $arr_length; $i++) {
-          echo '<li><a class="anchorTag" id="'; 
-          echo $menu_links[$i]; 
-          echo '"';
-          echo $menu_links[$i]; 
-          echo '</a></li>';
-        }
-        echo '</ul>
-      </nav>
-    </div>
-  </footer>
-  <script src="./scripts/material.js"></script>
-  <script src="./scripts/custom.js"></script>
+      <div class="container">
+        <nav class="grey darken-4">
+          <ul>';
+          for($i = 0; $i < $arr_length; $i++) {
+            echo '<li><a class="anchorTag" id="'; 
+            echo $menu_links[$i]; 
+            echo '"';
+            echo $menu_links[$i]; 
+            echo '</a></li>';
+          }
+          echo '</ul>
+        </nav>
+      </div>
+    </footer>
+    <script src="./scripts/material.js"></script>
+    <script src="./scripts/custom.js"></script>
 </body>
 </html>
     ';
--- web/fintech/main.php
+++ web/fintech/main.php
@@ -1,3 +1,2 @@
 <?php
-
 echo '<main class="container">';
