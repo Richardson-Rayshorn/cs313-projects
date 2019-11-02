c874b53ed992089ea4b729bc8e2d322e8fb572e2
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-02 19:47:27 -0400

    designing the app

--- web/fintech/footer.php
+++ web/fintech/footer.php
@@ -3,11 +3,20 @@
     </main>
     <footer class="grey darken-4">
       <div class="container">
-        <div id="footer-nav">
-          <ul>
-            <li>Signup</li>
-            <li>Login</li>
-          </ul>
+        <div id="footer-nav" class="row">
+          <a href="./" class="brand-logo left">FundMine</a>
+          <div class="col sm12 m6 l6">
+            <ul>
+              <li>Signup</li>
+              <li>Login</li>
+            </ul>
+          </div>
+          <div class="col sm12 m6 l6">
+            <ul>
+              <li>Signup</li>
+              <li>Login</li>
+            </ul>
+          </div>
         </div>
         <div id="copyright">
           <p>&copy Eknotec Services</p>
