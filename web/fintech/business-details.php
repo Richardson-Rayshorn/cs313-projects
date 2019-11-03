b884050827a79ad3690c745993b8082be92cf64b
RichardsonRayshorn <rayshorndc@gmail.com>
2019-11-02 22:51:52 -0400

    further updates

--- web/fintech/business-details.php
+++ web/fintech/business-details.php
@@ -8,17 +8,39 @@ include_once "header.php";
 include_once "main.php";
 
 echo $_GET['compId'];
-echo '
-<!-- give more details on each business
-and a button that users can click to add the 
-business to their list of interest -->
 
-<div >
-    <h3>Company Name</h3>
-    <p>Company desc</p>
-    <p>contact person</p>
-    <p>number</p>
-    <p>address</p>
-</div>';
+$query = $db->prepare('SELECT company_name, company_description, 
+    primary_contact_person, phone_number, company_address
+    FROM institutions');
+$query->execute();
+$business = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($business as $rows)
+{
+    if($_GET['compId'] == $rows['company_name'])
+    {
+        echo '
+        <!-- give more details on each business
+        and a button that users can click to add the 
+        business to their list of interest -->
+        <div class="col sm12">
+            <h5>';
+            echo $rows['company_name'];
+            echo '</h5>
+            <p> <span class="bold"> Description</span> <br/>';
+            echo $rows['company_description'];
+            echo '</p>
+            <p> <span class="bold"> Contact Person</span> <br/>';
+            echo $rows['primary_contact_person'];
+            echo '</p>
+            <p> <span class="bold"> Number</span> <br/>';
+            echo $rows['phone_number'];
+            echo '</p>
+            <p> <span class="bold"> Address</span> <br/>';
+            echo $rows['company_address'];
+            echo '</p>
+        </div>';
+    }
+}
 
 include_once "footer.php"; ?>
